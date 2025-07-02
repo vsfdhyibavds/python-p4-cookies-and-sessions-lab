31f1772aeb51e49ea402c614b321c9149f433181
Eugene Wekesa <eugenco578@gmail.com>
2025-07-02 16:45:48 +0300

    Use the session object to persist data across multiple requests.

--- server/app.py
+++ server/app.py
@@ -22,13 +22,23 @@ def clear_session():
 
 @app.route('/articles')
 def index_articles():
-
     pass
 
 @app.route('/articles/<int:id>')
 def show_article(id):
+    if 'page_views' not in session:
+        session['page_views'] = 0
 
-    pass
+    session['page_views'] += 1
+
+    if session['page_views'] <= 3:
+        article = Article.query.get(id)
+        if article:
+            return jsonify(article.to_dict()), 200
+        else:
+            return jsonify({'message': 'Article not found'}), 404
+    else:
+        return jsonify({'message': 'Maximum pageview limit reached'}), 401
 
 if __name__ == '__main__':
     app.run(port=5555)
